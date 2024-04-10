32e4b74cfc208407609a7a4c8ec01fc6de02eb97
Ciaran ONeill <ciaran@email.sc.edu>
2024-04-10 00:41:09 -0400

    Bug fixes

---
+++ fdsfdsf/src/main/java/com/example/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PrimaryController {
+
+    @FXML
+    private void handleButtonClick() {
+        System.out.println("Button clicked!");
+    }
+}

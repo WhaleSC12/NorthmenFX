a449978d6e4bf93e3c6d6156a22c4423d1386df6
tyler logue <tylerlogue3@gmail.com>
2024-04-17 15:17:01 -0400

    added advisor profile

---
+++ fdsfdsf/src/main/java/com/example/AdvisorProfile.java
@@ -0,0 +1,22 @@
+package com.example;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+public class AdvisorProfile {
+
+    @FXML
+    private ImageView homeButton;
+
+    @FXML
+    private ImageView searchButton;
+
+    @FXML
+    private ImageView calendarButton;
+
+    @FXML
+    private ImageView profileButton;
+
+    @FXML
+    private TextField advisorName;
+
+}

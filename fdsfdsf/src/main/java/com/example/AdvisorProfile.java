03963a9d88744cecc55757c13736abf3b2ad3a05
tyler logue <tylerlogue3@gmail.com>
2024-04-21 20:29:58 -0400

    advisor Proile controller

--- fdsfdsf/src/main/java/com/example/AdvisorProfile.java
+++ fdsfdsf/src/main/java/com/example/AdvisorProfile.java
@@ -3,6 +3,11 @@ package com.example;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+import com.DegreeEZ.*;
 
 public class AdvisorProfile {
 
@@ -20,6 +25,44 @@ public class AdvisorProfile {
 
     @FXML
     private TextField advisorName;
+
+    private User currentUser;
+
+    @FXML
+    public void initialize() {
+        advisorHomeButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> navigateTo("advisor-home.fxml"));
+        advisorSearchButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> navigateTo("advisor-audit.fxml"));
+        advisorCalendarButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> navigateTo("advisor-register.fxml"));
+        advisorProfileButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> navigateTo("advisor-profile.fxml"));
+        currentUser = DegreeWorksApplication.getInstance().getUser();
+        if(currentUser instanceof Advisor) {
+            Advisor advisor = (Advisor) currentUser;
+            updateProfile(advisor);
+        } else {
+            System.out.println("Current user is not an Advisor");
+        }
+    }
+
+    private void updateProfile(Advisor advisor) {
+        advisorName.setText(advisor.toString());
+    }
+
+    @FXML
+    private void signOut() {
+        DegreeWorksApplication.getInstance().logout();
+        navigateTo("primary.fxml");
+    }
+
+    @FXML
+    private void navigateTo(String fxmlFile) {
+        System.out.println("Navigating to " + fxmlFile);
+        try {
+            App.setRoot(fxmlFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to navigate to: " + fxmlFile);
+        }
+    }
 
 }
 
